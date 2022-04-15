bbae26eb8328915e2098d2b4c8531324b96b79a9
WhereIsLucas <lucas@retinens.com>
2022-04-15 10:50:53 +0200

    first commit

---
+++ 2022_04_15_104321_update_users_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name','first_name');
+            $table->string('last_name');
+            $table->string('type')->default('default');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('first_name','name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('type');
+        });
+    }
+};
