6e6521369e457847f49c09b88c735db1eefd5045
WhereIsLucas <lucas@retinens.com>
2023-03-11 11:39:45 +0100

    added seotools to base controller

--- common/Controller.php
+++ common/Controller.php
@@ -6,10 +6,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Artesaos\SEOTools\Traits\SEOTools;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests;
     use DispatchesJobs;
     use ValidatesRequests;
+    use SEOTools;
 }
